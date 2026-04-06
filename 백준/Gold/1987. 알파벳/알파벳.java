b0c7e09094906d835c8d3cc1d56c023a66882a4d
minjeongkimm <83492954+minjeongkimm@users.noreply.github.com>
2026-04-06 22:56:18 +0900

    [Gold IV] Title: 알파벳, Time: 824 ms, Memory: 14636 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 public class Main {
     static int R, C;
     static char[][] map;
-    static boolean[] checked = new boolean[26];
     static int ans;
 
     static int[] dr = {-1, 0, 1, 0};
@@ -29,14 +28,14 @@ public class Main {
 
         ans = 1;
         /// DFS
-        checked[map[0][0] - 'A'] = true;
-        dfs(0, 0, 1);
+
+        dfs(0, 0, 1, 1 << (map[0][0] - 'A'));
         System.out.println(ans);
 
     }// main
 
     // dfs
-    static void dfs(int cr, int cc, int cnt) {
+    static void dfs(int cr, int cc, int cnt, int mask) {
 
         // 최댓값 갱신
         ans = Math.max(ans, cnt);
@@ -49,13 +48,11 @@ public class Main {
             if (nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
 
             // 2. 방문 체크
-            char c = map[nr][nc];
-            if (checked[c - 'A']) continue;
+            int nAlpha = map[nr][nc] - 'A';
+            if ((mask & (1 << nAlpha)) != 0) continue;
 
             // 3. 위 조건 다 통과하면
-            checked[c - 'A'] = true;
-            dfs(nr, nc, cnt + 1);
-            checked[c - 'A'] = false;
+            dfs(nr, nc, cnt + 1, mask | 1 << nAlpha);
         }
 
     }//dfs
